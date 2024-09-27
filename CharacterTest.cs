5890123ed9f2675abf9cf3fa724679ca4aeb09fb
Felicia-Lindblad <lindblad.felicia@gmail.com>
2024-09-27 16:34:49 +0200

    Add project files.

---
+++ CharacterTest.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace SpelProject
+{
+    public interface ICharacter
+    {
+        public void Attack();
+    }
+
+    public class Player : ICharacter
+    {
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public Player(int health, int attackPower)
+        {
+            Health = health;
+            AttackPower = attackPower;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"You have attacked the enemy for {AttackPower} health");
+        }
+
+        public void Heal()
+        {
+            int healAmount = new Random().Next(1, 11);
+            Health += healAmount;
+            Console.WriteLine($"Player heals for {healAmount} health.");
+        }
+        
+    }
+
+    public class Enemy : ICharacter
+    {
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+
+        public Enemy(int health, int attackPower)
+        {
+            Health = health;
+            AttackPower = attackPower;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"You have been attacked for {AttackPower} health");
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SpelProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            playerOption();
+
+            Player player = new Player(10, 2);
+            Enemy enemy = new Enemy(8, 1);
+
+            string optinChoosed = Console.ReadLine()!;
+
+            if (optinChoosed == "a")
+            {
+                player.Attack();
+            } else if (optinChoosed == "h")
+            {
+                player.Heal(); 
+            }
+            
+            
+            
+        }
+
+        static void playerOption()
+        {
+            Console.WriteLine("Let the game begin");
+            Console.WriteLine("Choose your action");
+            Console.WriteLine("a - attack");
+            Console.WriteLine("h - heal");
+        }
+    }
+}
